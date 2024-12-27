6ff251ce4973d74b2994a43ad071fac5f2742d2e
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-27 23:28:07 +0500

    Использование тренарных операций

---
+++ print_with_if.py
@@ -0,0 +1,13 @@
+# Проверка использования тренарного оператора
+# https://youtube.com/shorts/yymyLXPbP1Y?si=MwvZcYzASw98OWTr
+
+number = 3
+print("Even" if number % 2 == 0 else "Odd")
+
+
+
+# https://pavel-karateev.gitbook.io/intermediate-python/sintaksis/ternary_operators
+func_output = "Все посчитано!"
+msg = func_output or "Не было возвращено данных"
+print(msg)
+# Вывод: Не было возвращено данных

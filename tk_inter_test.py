5bbf408145d4c2ee4c972cbe6b189ecf84ce953d
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-27 23:50:34 +0500

    Переименовал файл для более удобного опознования. Прикрепил обработку event

---
+++ tk_inter_test.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+def Hello(event):
+    print("Hello world!!!")
+
+btn = Button(
+    root,  # родительское окно
+    text="Click me",  # Надпись на кнопке
+    width=30, height=5,  # размеры кнопки ширина и высота
+    bg="white", fg="black"  # цвет фона и цвет надписи 
+)
+btn.bind("<Button-1>", Hello)
+btn.pack()
+root.mainloop()
